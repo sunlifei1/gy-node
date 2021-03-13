b07e7173866e2037b9767c94c742f6b6b10f07c1
sun.lf <sunlifei@126.com>
2021-03-13 20:57:51 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,68 @@ var Mock = require('mockjs')
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
-    var data = Mock.mock({
-        'list|1-10': [
-            {
-                'id|+1': 1,
-            },
-        ],
-    })
+    let data = [
+        {
+            id: 1,
+            url: 'https://www.baidu.com',
+            title: '每日逛',
+            sonItem: [
+                {
+                    title: 99,
+                    id: 99,
+                    url: 'https://www.baidu.com',
+                },
+            ],
+        },
+        {
+            id: 2,
+            url: 'https://www.fuliba2020.net',
+            title: '福利吧',
+        },
+        {
+            title: '百度',
+            id: 3,
+            url: 'https://www.baidu.com',
+            title: '百度',
+        },
+        {
+            title: '吾爱破',
+            id: 4,
+            url: 'https://www.52pojie.cn/index.php',
+        },
+        {
+            title: '小刀网',
+            id: 5,
+            url: 'https://www.xd0.com',
+        },
+        {
+            title: 6,
+            id: 6,
+            url: 'https://www.baidu.com',
+        },
+        {
+            title: 7,
+            id: 7,
+            url: 'https://www.baidu.com',
+        },
+        {
+            title: 8,
+            id: 8,
+            url: 'https://www.baidu.com',
+        },
+        {
+            title: 9,
+            id: 9,
+            url: 'https://www.baidu.com',
+        },
+        {
+            title: 10,
+            id: 10,
+            url: 'https://www.baidu.com',
+        },
+    ]
     // res.send('发送文本格式的内容')   //发送文本格式的内容
-    res.json({ title: data }) //发送json格式的内容
+    res.json({ data: data, code: 200, msg: 'success' }) //发送json格式的内容
 })
 
 module.exports = router
