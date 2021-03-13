551b8a288e9c009e4d58ff6c465e0e5144bc1e44
sun.lf <sunlifei@126.com>
2021-03-13 20:47:53 +0800

    1

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.all('', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '')
+    res.header('Access-Control-Allow-Origin', '*')
 
     //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
 
